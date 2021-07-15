b27e9e68402fb8c9a62ab0614f97d6a84be69e76
Sang T. Truong <sttruong@stanford.edu>
2021-07-15 13:04:47 -0400

    Update getDecile.py

--- incomevis/utils/getDecile.py
+++ incomevis/utils/getDecile.py
@@ -1,4 +1,4 @@
 def getDecile(type):
-    if type == 'numeric': [0.05, 0.15, 0.25, 0.35, 0.45, 0.50, 0.55, 0.65, 0.75, 0.85, 0.95]
+    if type == 'numeric': return [0.05, 0.15, 0.25, 0.35, 0.45, 0.50, 0.55, 0.65, 0.75, 0.85, 0.95]
     elif type == 'string': return ['5p','15p','25p','35p','45p','50p','55p','65p','75p','85p','95p']
     else: raise ValueError
