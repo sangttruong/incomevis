dbc45c79b9ae1170ca9177eecd82f046b38f40ca
Sang T. Truong <sttruong@stanford.edu>
2021-07-15 13:04:45 -0400

    Update getStateName.py

--- incomevis/utils/getStateName.py
+++ incomevis/utils/getStateName.py
@@ -9,5 +9,7 @@ def getStateName(type):
                                 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
                                 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia',
                                 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
-    elif type == 'numeric': return 
+    elif type == 'numeric': return [1, 2, 4, 5, 6, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20,
+                                    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+                                     37, 38, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56]
     else: raise ValueError
