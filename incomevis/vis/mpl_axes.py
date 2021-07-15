57e387a2fdd82c46fa31c16e5714a894c3bcaf95
Sang T. Truong <sttruong@stanford.edu>
2021-07-15 13:11:41 -0400

    Update mpl_axes.py

--- incomevis/vis/mpl_axes.py
+++ incomevis/vis/mpl_axes.py
@@ -1,9 +1,6 @@
 def axis_config(ax, axes_config):
     ax.view_init(5,-146)
-    axes_dict = {'xaxis': [ax.set_xlim, ax.set_xticks, ax.set_xticklabels, ax.set_xlabel, ax.xaxis],
-                 'yaxis': [ax.set_ylim, ax.set_yticks, ax.set_yticklabels, ax.set_ylabel, ax.yaxis],
-                 'zaxis': [ax.set_zlim, ax.set_zticks, ax.set_zticklabels, ax.set_zlabel, ax.zaxis]}
-
+    # parsing arguments:
     for axis in axes_config.key:
         axes_config[axis][0](config_dict[axis][0][0], config_dict[axis][0][1]) 
         ax.set_zticks(zticks)
