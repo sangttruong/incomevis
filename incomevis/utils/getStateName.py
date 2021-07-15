1004a52d56b543a91e3432699cdd399a9836164d
Sang T. Truong <sttruong@stanford.edu>
2021-07-15 01:24:14 -0400

    allow access state information via name (string) or numeric (statefip)

--- incomevis/utils/getStateName.py
+++ incomevis/utils/getStateName.py
@@ -1,11 +1,13 @@
-def getStateName():
-    return ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-            'Colorado', 'Connecticut', 'Delaware', 'District of Columbia',
-            'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana',
-            'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
-            'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri',
-            'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
-            'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
-            'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
-            'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia',
-            'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+def getStateName(type):
+    if type == 'string': return ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+                                'Colorado', 'Connecticut', 'Delaware', 'District of Columbia',
+                                'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana',
+                                'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+                                'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri',
+                                'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
+                                'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
+                                'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
+                                'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia',
+                                'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+    elif type == 'numeric': return 
+    else: raise ValueError
