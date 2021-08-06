06c8a2b7be71755c71845b9a4f44b97f0e41627c
Sang T. Truong <sttruong@stanford.edu>
2021-08-06 19:46:47 -0400

    Update mpl_colorbar.py

--- incomevis/vis/mpl_colorbar.py
+++ incomevis/vis/mpl_colorbar.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import matplotlib
+
 def colorbar_config(fig, left=0.721, elevation=0.62, width=0.02, height=0.2, alpha = 0,
                     cm_str='bwr', upper = 40000, lower=-60000):
   """
@@ -9,13 +12,13 @@ def colorbar_config(fig, left=0.721, elevation=0.62, width=0.02, height=0.2, alp
 
       left: adjust left (decrease) and right (increase);
             Default: 0.73
-      
+
       elevation: adjust down (decrease) and up (increase);
             Default: 0.65
-      
+
       width: width of the colorbar;
             Default: 0.02
-      
+
       height: height of the colorbar;
             Default: 0.2
 
@@ -27,7 +30,7 @@ def colorbar_config(fig, left=0.721, elevation=0.62, width=0.02, height=0.2, alp
 
       upper: upper bound difference;
             Default: 40000
-      
+
       lower: lower bound difference;
             Default: -60000
   """
@@ -37,5 +40,5 @@ def colorbar_config(fig, left=0.721, elevation=0.62, width=0.02, height=0.2, alp
   cmap = plt.cm.get_cmap(cm_str).reversed()
   norm = matplotlib.colors.TwoSlopeNorm(vmin=-60000, vmax=40001, vcenter=0)
   cb = fig.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap), cax=cbax, shrink = 0.15, ticks=[-60000,0,40000])
-  cb.ax.set_yticklabels([str(lower) , '0 (50p benchmark)', str(upper)], fontsize=15, weight='bold') 
+  cb.ax.set_yticklabels([str(lower) , '0 (50p benchmark)', str(upper)], fontsize=15, weight='bold')
   return cb
