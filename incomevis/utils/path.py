d37a0d21cfa39b1596c96932095d812e241b81ff
Sang T. Truong <sttruong@stanford.edu>
2021-08-06 21:55:07 -0400

    Update path.py

--- incomevis/utils/path.py
+++ incomevis/utils/path.py
@@ -2,12 +2,17 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname("__file__"), '..'))
 sys.path.append(os.path.join(os.path.dirname("__file__"), '..', '..'))
 
-if 'google.colab' in str(get_ipython()):
-    SOURCE_DATA_PATH = '/content/incomevis/data/'
-    BENCHMARK_PATH = ''
-    DATA_PATH =
-else:
-    SOURCE_DATA_PATH =
-    BENCHMARK_PATH =
-    DATA_PATH =
-    
+def get_root_dir():
+    dirname = os.getcwd()
+    dirname_split = dirname.split("/")
+    index = dirname_split.index("incomevis")
+    dirname = "/".join(dirname_split[:index + 1])
+    return dirname
+
+if 'google.colab' in str(get_ipython()): root = '/content/incomevis'
+else: root = get_root_dir()
+
+SOURCE_DATA_PATH    = root + '/data/source_data/'
+BENCHMARK_DATA_PATH = root + '/data/benchmark_data/'
+DEFLATED_DATA_PATH  = root + '/data/deflated_data/'
+OUTPUT_DATA_PATH    = root + '/data/output_data/'
