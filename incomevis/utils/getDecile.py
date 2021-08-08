4be2b356a7b6eedf6046dd27529934e16f16a2a7
Sang T. Truong <sttruong@stanford.edu>
2021-08-07 23:56:46 -0400

    Update getDecile.py

--- incomevis/utils/getDecile.py
+++ incomevis/utils/getDecile.py
@@ -18,11 +18,12 @@ def getDecile(type):
         ----------
         type: str
             Type of decile. Currently supported ``'string'`` (e.g. ``'5p'``) or
-            ``'numeric'`` (e.g. ``0.5``)
+            ``'numeric'`` (e.g. ``0.5``).
 
         Returns
-        -------
+        ----------
         class 'list'
+            A list of decile.
 
     """
     if type == 'numeric': return [0.05, 0.15, 0.25, 0.35, 0.45, 0.50, 0.55, 0.65, 0.75, 0.85, 0.95]
