e0e1f8158291a19ee8162efad92db8f3eb71f88a
Sang T. Truong <sttruong@stanford.edu>
2021-08-10 12:08:16 -0400

    Update visualize.py

--- incomevis/vis/visualize.py
+++ incomevis/vis/visualize.py
@@ -20,8 +20,9 @@ def visualize(k = 'decile', input_path = ''):
   """
     Get interactive visualization in AmChart. Receive deflated data of one year with
     normalized (potentially unrounded) population with details. The data is assumed to
-    have (1) an index column using ``State``, (3) kiles name, (4) ``Label``, (5) ``Color``, 
-    and (6) ``NORMPOP``. Regardless of the state order in the input, the output will always 
+    have (1) an index column using ``State``, (2) kiles name, (3) ``NORMPOP``, (4) ``Label``,
+    and (5) ``Color`` in hexadecimal form (e.g. #FFE000). There is no particular requirement on
+    order of these fields. Regardless of the state order in the input, the output will always
     be sorted (left to right) according to the ``50p``.
 
     Parameters
@@ -46,7 +47,7 @@ def visualize(k = 'decile', input_path = ''):
   # Convert csv to json format
   result = pd.read_csv(input_path, index_col = 'State')
 
-  # use statename from the data instead of getStateName('string') function 
+  # use statename from the data instead of getStateName('string') function
   # to allow user to make mistake of naming state wrong (like DC instead of District of Columbia)
   statenamee = result.index.tolist()
 
